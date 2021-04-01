3675e28ddd9ec8f726b3945ff43bcc0fba0debee
Dimon <dimka40302010@gmail.com>
2021-04-01 13:24:46 +0300

    geo

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var result;
+const api_key = 'bed950b3229a2b9bc8677bb8c28d5508';
 
 window.onload = function() {
     result = document.getElementById('result');
@@ -9,19 +10,28 @@ window.onload = function() {
 		// Передаем две функции
 		navigator.geolocation.getCurrentPosition(
 		            geolocationSuccess, geolocationFailure);
-		
-		// Выводим результат
-		result.innerHTML = "Поиск начался";
 	}
 	else {
-		// Выводим результат
-		result.innerHTML = "Ваш браузер не поддерживает геолокацию";
 	}
 }
 
 function geolocationSuccess(position) {
-	result.innerHTML = "Последний раз вас засекали здесь: " +
-	         position.coords.latitude + ", " + position.coords.longitude;
+	CoordLink = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${api_key}&lang=ru`;
+	
+	fetch(CoordLink)
+		.then(function(resp) {return resp.json()})
+		.then(function(data){
+			
+			document.querySelector('.geo_city_title').textContent = data.name;
+			document.querySelector('.temperature').innerHTML = Math.round(data.main.temp - 273) + '&deg;C'; 
+			document.querySelector('.wind').textContent = (data.wind.speed) + ' м/с, ' + convertWind(data.wind.deg);
+			document.querySelector('.cloud').textContent = (data.clouds.all) + '%' ;
+			document.querySelector('.pressure').textContent = (data.main.pressure) + ' мм.рт.ст.' ;
+			document.querySelector('.humidity').textContent = (data.main.humidity) + '%' ;
+			document.querySelector('.coordinates').textContent = '[' + (data.coord.lat) + ' , ' + (data.coord.lon) + ']' ;
+			document.querySelector('.geo_city_img').src = "https://openweathermap.org/img/wn/" + (data.weather[0].icon) + "@2x.png";
+		}
+		)
 }
 
 function geolocationFailure(positionError) {
@@ -41,4 +51,21 @@ function geolocationFailure(positionError) {
 	else {
 		result.innerHTML = "Загадочная ошибка.";
 	}
+}
+
+function convertWind (wind){
+	const dirs = {N: 'С', W: 'З', E: 'В', S: 'Ю'};
+	let result = '';
+
+	if (wind === 0) {result += dirs.N;}
+	if ((wind > 0) && (wind <= 45) ) {result += dirs.N + '/' + dirs.E;}
+	if ((wind > 45) && (wind <= 90) ) {result += dirs.E;}
+	if ((wind > 90) && (wind <= 135) ) {result += dirs.E + '/' + dirs.S;}
+	if ((wind > 135) && (wind <= 180) ) {result +=dirs.S;}
+	if ((wind > 180) && (wind <= 225) ) {result += dirs.S + '/' + dirs.W;}
+	if ((wind > 225) && (wind <= 270) ) {result +=dirs.W;}
+	if ((wind > 270) && (wind <= 315) ) {result += dirs.N + '/' + dirs.W;}
+	if ((wind > 315) && (wind <= 360) ) {result +=dirs.N;}
+
+	return result;
 }
