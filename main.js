2f8426b94a3152692ef743235a19016bc743183f
Dimon <dimka40302010@gmail.com>
2021-04-01 13:09:36 +0300

    new js

--- main.js
+++ main.js
@@ -1,74 +1,44 @@
-const api_key = 'bed950b3229a2b9bc8677bb8c28d5508';
-
-geoFindMe()
+var result;
 
 window.onload = function() {
-  var startPos;
-  var geo = function(position) {
-    startPos = position;
-    const lat = document.getElementById('startLat').textContent = startPos.coords.latitude;
-    const lon = document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-  };
+    result = document.getElementById('result');
+	
+	// Если функциональность геолокации доступна, 
+	// пытаемся определить координаты посетителя
+	if (navigator.geolocation) {
+		// Передаем две функции
+		navigator.geolocation.getCurrentPosition(
+		            geolocationSuccess, geolocationFailure);
+		
+		// Выводим результат
+		result.innerHTML = "Поиск начался";
+	}
+	else {
+		// Выводим результат
+		result.innerHTML = "Ваш браузер не поддерживает геолокацию";
+	}
 }
 
-function geoFindMe() {
-  var startPos;
-  var geoSuccess = function(position) {
-	startPos = position;
-	
-	
-	console.log(lat);
-	console.log(lon);
-
-	CoordLink = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&lang=ru`;
-	
-	fetch(CoordLink)
-		.then(function(resp) {return resp.json()})
-		.then(function(data){
-			
-			document.querySelector('.geo_city_title').textContent = data.name;
-			document.querySelector('.temperature').innerHTML = Math.round(data.main.temperature - 273) + '&deg;C'; 
-			document.querySelector('.wind').textContent = (data.wind.speed) + ' м/с, ' + convertWind(data.wind.deg);
-			document.querySelector('.cloud').textContent = (data.clouds.all) + '%' ;
-			document.querySelector('.pressure').textContent = (data.main.pressure) + ' мм.рт.ст.' ;
-			document.querySelector('.humidity').textContent = (data.main.humidity) + '%' ;
-			document.querySelector('.coordinates').textContent = '[' + (data.coord.lat) + ' , ' + (data.coord.lon) + ']' ;
-			document.querySelector('.geo_city_img').src = "https://openweathermap.org/img/wn/" + (data.weather[0].icon) + "@2x.png";
-		}
-		)
-  };
-  
-  var geoError = function(position) {
-    console.log('Error occurred. Error code: ' + error.code);
-    // error.code can be:
-    //   0: unknown error
-    //   1: permission denied
-    //   2: position unavailable (error response from location provider)
-    //   3: timed out
-  };
-  
-  navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
-
-};
-
-function convertWind (wind){
-	const dirs = {N: 'С', W: 'З', E: 'В', S: 'Ю'};
-	let result = '';
-
-	if (wind === 0) {result += dirs.N;}
-	if ((wind > 0) && (wind <= 45) ) {result += dirs.N + '/' + dirs.E;}
-	if ((wind > 45) && (wind <= 90) ) {result += dirs.E;}
-	if ((wind > 90) && (wind <= 135) ) {result += dirs.E + '/' + dirs.S;}
-	if ((wind > 135) && (wind <= 180) ) {result +=dirs.S;}
-	if ((wind > 180) && (wind <= 225) ) {result += dirs.S + '/' + dirs.W;}
-	if ((wind > 225) && (wind <= 270) ) {result +=dirs.W;}
-	if ((wind > 270) && (wind <= 315) ) {result += dirs.N + '/' + dirs.W;}
-	if ((wind > 315) && (wind <= 360) ) {result +=dirs.N;}
-
-	return result;
+function geolocationSuccess(position) {
+	result.innerHTML = "Последний раз вас засекали здесь: " +
+	         position.coords.latitude + ", " + position.coords.longitude;
 }
 
-function refreshPage(){
-    window.location.reload();
-} 
+function geolocationFailure(positionError) {
+	if(positionError == 1) {
+		result.innerHTML = "Вы решили не предоставлять данные о своем местоположении, " + 
+		        "но это не проблема. Мы больше не будем запрашивать их у вас.";
+	}
+	else if(positionError == 2) {
+		result.innerHTML = "Проблемы с сетью или нельзя связаться со службой определения " + 
+		        "местоположения по каким-либо другим причинам.";
+	}
+	else if(positionError == 3) {
+		result.innerHTML = "He удалось определить местоположение " 
+		        + "в течение установленного времени. ";
 
+	}
+	else {
+		result.innerHTML = "Загадочная ошибка.";
+	}
+}
